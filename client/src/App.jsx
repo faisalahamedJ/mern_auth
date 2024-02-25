ff74ccc98df295711194c8a7e36bcf2f7ec27133
faisal ahamed j <faisalahamed5002@gmail.com>
2024-02-25 17:43:57 +0530

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+// import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 export default function App() {
-  return <h6 className="text-red-600 font-extrabold text-3xl">App</h6>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/sign-up" element={SignUp} /> */}
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
