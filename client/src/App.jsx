28e78419430530af1efb36711c664027409fcc8e
faisal ahamed j <faisalahamed5002@gmail.com>
2024-02-25 18:49:06 +0530

    create & run the server using nodemon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-// import SignUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 
 export default function App() {
@@ -8,7 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/sign-up" element={SignUp} /> */}
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
